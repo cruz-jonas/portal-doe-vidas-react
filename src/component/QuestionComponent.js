bd6a0b8b9bc969fd2c1b9badfed87bb93ee22017
JonasSCruz <jonas.cruz@dbccompany.com.br>
2023-04-21 16:31:19 -0300

    add url variable

--- src/component/QuestionComponent.js
+++ src/component/QuestionComponent.js
@@ -3,7 +3,7 @@ import { questions } from '../data/questions'
 
 const QuestionComponent = ({ addChoice }) => {
 
-    const url = "https://localhost:5000/api/user-answers/register"
+    const url = process.env.URL+"/api/user-answers/register"
 
     const [currentPosition, setCurrentPosition] = useState(0);
     const [selected, setSelected] = useState("")
